ced292f9b7a93ef873009650a88c3c2ed101560c
serek8 <serek8@gmail.com>
2015-01-30 01:32:31 +0100

    s1

---
+++ onescript.js
@@ -0,0 +1,40 @@
+var slideMapButtonVisibility=0;
+var panelToChange;
+$(document).ready(function(){
+  $(".slidebutton").click(function(){
+	panelToChange="#panel"+(this.id);
+	
+    if(slideMapButtonVisibility == 0){
+		$(panelToChange).slideDown("slow");
+		slideMapButtonVisibility=1;
+	}
+	else{
+	$(panelToChange).slideUp("slow");
+	slideMapButtonVisibility = 0;
+	}
+  });
+});
+
+
+/* Skrypt do mapy */
+var myCenter=new google.maps.LatLng(51.508742,-0.120850);
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:5,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+/* Koniec skryptu mapy */
